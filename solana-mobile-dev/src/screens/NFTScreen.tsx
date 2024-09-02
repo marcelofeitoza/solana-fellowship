d94fa72bcbf346f25130390b5f3dbfd5d778effc
Marcelo G Feitoza <feitoza.marcelo7@gmail.com>
2024-09-02 20:24:44 -0300

    feat: WIP of solana mobile dev exercise

---
+++ solana-mobile-dev/src/screens/NFTScreen.tsx
@@ -0,0 +1,247 @@
+import React, { useRef, useState } from "react";
+import {
+	Button,
+	Image,
+	ScrollView,
+	StyleSheet,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from "react-native";
+import { Icon, Text } from "react-native-paper";
+import * as Location from "expo-location";
+
+import { Section } from "../Section";
+import {
+	Camera,
+	CameraView,
+	CameraType,
+	useCameraPermissions,
+} from "expo-camera";
+import MaterialCommunityIcon from "@expo/vector-icons/MaterialCommunityIcons";
+
+export function NFTScreen() {
+	const [facing, setFacing] = useState<CameraType>("front");
+	const [permission, requestPermission] = useCameraPermissions();
+	const [image, setImage] = useState<string | null>(null);
+	const cameraRef = useRef<Camera | null>(null);
+	const [formData, setFormData] = useState({
+		name: "",
+		description: "",
+		location: "",
+	});
+
+	const toggleCameraFacing = () => {
+		setFacing((prev) => (prev === "back" ? "front" : "back"));
+	};
+
+	const takePicture = async () => {
+		if (cameraRef.current) {
+			const photo = await cameraRef.current.takePictureAsync({
+				quality: 1,
+			});
+			setImage(photo.uri);
+		}
+	};
+
+	const handleInputChange = (name: string, value: string) => {
+		setFormData({ ...formData, [name]: value });
+	};
+
+	const handleRemoveImage = () => {
+		setImage(null);
+	};
+
+	const getCoordinates = async () => {
+		let { status } = await Location.requestForegroundPermissionsAsync();
+		if (status !== "granted") {
+			alert("Permission to access location was denied");
+			return;
+		}
+
+		let location = await Location.getCurrentPositionAsync({});
+		setFormData({
+			...formData,
+			location: `${location.coords.latitude}, ${location.coords.longitude}`,
+		});
+	};
+
+	return (
+		<ScrollView style={styles.screenContainer}>
+			<Text
+				style={{ fontWeight: "bold", marginBottom: 12 }}
+				variant="displaySmall"
+			>
+				NFT Minting
+			</Text>
+
+			{/* Form */}
+			<Section
+				title="NFT Details"
+				description="Enter the details of your NFT."
+			/>
+
+			<TextInput
+				style={styles.input}
+				placeholder="Name"
+				placeholderTextColor={"gray"}
+				value={formData.name}
+				onChangeText={(text) => handleInputChange("name", text)}
+			/>
+			<TextInput
+				style={styles.input}
+				placeholder="Description"
+				placeholderTextColor={"gray"}
+				value={formData.description}
+				onChangeText={(text) => handleInputChange("description", text)}
+			/>
+			<View style={styles.buttonRow}>
+				<TextInput
+					style={{ ...styles.input, marginBottom: 0 }}
+					placeholder="Location"
+					placeholderTextColor={"gray"}
+					value={formData.location}
+					editable={false}
+				/>
+				<TouchableOpacity
+					onPress={getCoordinates}
+					style={{
+						...styles.button,
+						backgroundColor: "darkgreen",
+						padding: 8,
+						marginTop: 0,
+						alignSelf: "center",
+					}}
+				>
+					<Text style={styles.text}>Get Location</Text>
+				</TouchableOpacity>
+			</View>
+
+			{permission?.granted ? (
+				image ? (
+					<View>
+						<Image
+							source={{ uri: image }}
+							style={{ width: "100%", height: 500 }}
+						/>
+						<TouchableOpacity
+							style={{
+								...styles.trashButton,
+							}}
+							onPress={handleRemoveImage}
+						>
+							<Text style={styles.text}>
+								<MaterialCommunityIcon
+									name={"trash-can"}
+									size={24}
+								/>
+							</Text>
+						</TouchableOpacity>
+					</View>
+				) : (
+					<CameraView
+						ref={cameraRef}
+						style={styles.camera}
+						facing={facing}
+					>
+						<View style={styles.buttonContainer}>
+							<TouchableOpacity
+								style={styles.button}
+								onPress={toggleCameraFacing}
+							>
+								<Text style={styles.text}>Flip Camera</Text>
+							</TouchableOpacity>
+
+							<TouchableOpacity
+								style={styles.button}
+								onPress={takePicture}
+							>
+								<Text style={styles.text}>Take Picture</Text>
+							</TouchableOpacity>
+						</View>
+					</CameraView>
+				)
+			) : (
+				<View style={styles.buttonGroup}>
+					<Button
+						onPress={requestPermission}
+						title="Request Camera Permission"
+					/>
+				</View>
+			)}
+
+			<View style={{ ...styles.buttonGroup, marginBottom: 16 }}>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={() => console.log(formData)}
+				>
+					<Text style={styles.text}>Mint NFT</Text>
+				</TouchableOpacity>
+			</View>
+		</ScrollView>
+	);
+}
+
+const styles = StyleSheet.create({
+	screenContainer: {
+		paddingHorizontal: 16,
+		paddingVertical: 32,
+		flex: 1,
+	},
+	buttonGroup: {
+		flexDirection: "column",
+		paddingVertical: 4,
+		marginBottom: 16,
+	},
+	camera: {
+		display: "flex",
+		height: 500,
+		width: "100%",
+	},
+	buttonContainer: {
+		flex: 1,
+		backgroundColor: "transparent",
+		flexDirection: "row",
+		margin: 20,
+		justifyContent: "space-between",
+	},
+	button: {
+		backgroundColor: "rgba(0, 0, 0, 0.5)",
+		padding: 16,
+		borderRadius: 8,
+		alignSelf: "flex-end",
+		alignItems: "center",
+	},
+	buttonRow: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		gap: 10,
+		alignContent: "center",
+		alignItems: "center",
+		marginBottom: 16,
+	},
+	trashButton: {
+		backgroundColor: "black",
+		position: "absolute",
+		padding: 8,
+		opacity: 0.5,
+		borderRadius: 8,
+		alignSelf: "flex-end",
+		alignItems: "center",
+		top: 16,
+		right: 16,
+	},
+	text: {
+		fontSize: 18,
+		color: "white",
+	},
+	input: {
+		height: 40,
+		borderColor: "gray",
+		color: "white",
+		borderWidth: 1,
+		marginBottom: 12,
+		padding: 8,
+		flex: 1,
+	},
+});
